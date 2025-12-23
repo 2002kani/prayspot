5dbcad9165aaf29c6fd907efd8647e0201093438
02Kani <kanikaradag02@gmail.com>
2025-12-23 14:20:52 +0100

    adding backend

---
+++ prayspot-backend/src/main/java/com/prayspot/prayspot_backend/PrayspotBackendApplication.java
@@ -0,0 +1,13 @@
+package com.prayspot.prayspot_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrayspotBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrayspotBackendApplication.class, args);
+	}
+
+}
---
+++ prayspot-backend/src/test/java/com/prayspot/prayspot_backend/PrayspotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prayspot.prayspot_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrayspotBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
