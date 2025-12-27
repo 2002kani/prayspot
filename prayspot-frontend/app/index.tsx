4552422f32117ab79c79a8fda83566a4d3be6309
Duha <DuhaK43@icloud.com>
2025-12-27 23:45:04 +0100

    Test Styling

--- prayspot-frontend/app/index.tsx
+++ prayspot-frontend/app/index.tsx
@@ -1,15 +1,24 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen. TEST.</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>
+        Edit app/index.tsx to edit this screen. TEST.
+      </Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#E8F5E9",
+  },
+  text: {
+    fontSize: 16,
+    color: "#2E7D32",
+  },
+});
